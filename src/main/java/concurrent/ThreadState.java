a694adc825a064b0ca94288b063e66dbf0f331f6
Fut <shmopls@gmail.com>
2022-09-13 16:23:00 +0500

    Состояние нити

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package concurrent;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println("First thread")
+        );
+        Thread second = new Thread(
+                () -> System.out.println("Second thread")
+        );
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+            System.out.println(second.getState());
+        }
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+    }
+}
