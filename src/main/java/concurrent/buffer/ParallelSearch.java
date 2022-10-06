e015ac2a2a4ef38bec7aaa3a632c6778dca2f1f5
Fut <shmopls@gmail.com>
2022-10-06 09:56:03 +0500

    Обеспечить остановку потребителя

---
+++ src/main/java/concurrent/buffer/ParallelSearch.java
@@ -0,0 +1,42 @@
+package concurrent.buffer;
+
+import concurrent.blockingqueue.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        );
+        producer.start();
+        consumer.join();
+        producer.join();
+    }
+}
