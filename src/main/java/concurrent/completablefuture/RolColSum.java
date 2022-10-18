4358cd6823db7019c779e4c50526840de4e7c262
Fut <shmopls@gmail.com>
2022-10-18 14:08:02 +0500

    CompletableFuture

--- src/main/java/concurrent/completablefuture/RolColSum.java
+++ src/main/java/concurrent/completablefuture/RolColSum.java
@@ -1,6 +1,8 @@
 package concurrent.completablefuture;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -9,44 +11,37 @@ public class RolColSum {
     public static Sums[] sum(int[][] matrix) {
         Sums[] sumArr = new Sums[matrix.length];
         for (int x = 0; x < matrix.length; x++) {
-            int countX = 0;
-            int countY = 0;
-            for (int y = 0; y < matrix.length; y++) {
-                countX += matrix[y][x];
-                countY += matrix[x][y];
-            }
-            Sums sum = new Sums(countX, countY);
-            sumArr[x] = sum;
+            sumArr[x] = lineSum(matrix, x);
         }
         System.out.println(Arrays.toString(sumArr));
         return sumArr;
     }
 
     public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
-        Sums[] sums = new Sums[matrix.length];
+        Map<Integer, CompletableFuture<Sums>> data = new HashMap<>();
+        Sums[] sumsArray = new Sums[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            int index = i;
-            CompletableFuture<Integer> sumRow = CompletableFuture.supplyAsync(() -> RolColSum.sumCol(matrix, index));
-            CompletableFuture<Integer> sumCol = CompletableFuture.supplyAsync(() -> RolColSum.sumRow(matrix, index));
-            sums[i] = new Sums(sumRow.get(), sumCol.get());
+            data.put(i, getSums(matrix, i));
         }
-        System.out.println(Arrays.toString(sums));
-        return sums;
+        for (Integer key : data.keySet()) {
+            sumsArray[key] = data.get(key).get();
+        }
+        return sumsArray;
     }
 
-    private static int sumCol(int[][] matrix, int rowIndex) {
-        int rsl = 0;
-        for (int col = 0; col < matrix.length; col++) {
-            rsl += matrix[col][rowIndex];
-        }
-        return rsl;
+    public static CompletableFuture<Sums> getSums(int[][] matrix, int index) {
+        return CompletableFuture.supplyAsync(
+                () -> lineSum(matrix, index)
+        );
     }
 
-    private static int sumRow(int[][] matrix, int colIndex) {
-        int rsl = 0;
-        for (int row = 0; row < matrix.length; row++) {
-            rsl += matrix[colIndex][row];
-        }
-        return rsl;
+    private static Sums lineSum(int[][] matrix, int index) {
+            int countX = 0;
+            int countY = 0;
+            for (int y = 0; y < matrix.length; y++) {
+                countX += matrix[y][index];
+                countY += matrix[index][y];
+            }
+            return new Sums(countX, countY);
     }
 }
